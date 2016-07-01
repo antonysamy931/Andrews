df873835bdb9386998f68aa2f8fa21dc494db353
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2016-07-01 17:09:17 +0530

    Base mvc structure

--- Admin/Tinytots.English/src/Tinytots.English.Master/Startup.cs
+++ Admin/Tinytots.English/src/Tinytots.English.Master/Startup.cs
@@ -5,22 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Configuration;
 
 namespace Tinytots.English.Master
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
